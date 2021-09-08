c550b2797675075297a05bfe20b3b4ef01838058
Hemna <waboring@hemna.com>
2021-09-08 13:50:20 -0400

    Added pre-commit hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,11 @@ import subprocess
 
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
+# This first one will fail due to pre-commit hook failing, which
+# will reformat some files to be compliant.
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+# non-compliant files have been modified, so just commit -a
+subprocess.call(['git', 'commit', '-a', '-m', 'Initial commit'])
+
 version = "{{cookiecutter.version}}"
 subprocess.call(['git', 'tag', '-a', version, '-m', 'Initial commit'])
