da97ae4d5a4e7d66a61cc02b5ca9cbc9ec6f2cef
Hemna <waboring@hemna.com>
2021-09-09 16:14:31 -0400

    Added the license file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,33 @@
 import subprocess
 
-subprocess.call(['git', 'init'])
-subprocess.call(['git', 'add', '*'])
-# This first one will fail due to pre-commit hook failing, which
-# will reformat some files to be compliant.
-subprocess.call(['git', 'commit', '-m', 'Initial commit'])
-# non-compliant files have been modified, so just commit -a
-subprocess.call(['git', 'commit', '-a', '-m', 'Initial commit'])
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-version = "{{cookiecutter.version}}"
-subprocess.call(['git', 'tag', '-a', version, '-m', 'Initial commit'])
+import logging
+import os
+import shutil
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("post_gen_project")
+
+ALL_TEMP_FOLDERS = ["licenses"]
+
+def remove_temp_folders(temp_folders):
+    for folder in temp_folders:
+        logger.info("Remove temporary folder: %s", folder)
+        shutil.rmtree(folder)
+
+
+if __name__ == "__main__":
+    remove_temp_folders(ALL_TEMP_FOLDERS)
+
+    subprocess.call(['git', 'init'])
+    subprocess.call(['git', 'add', '*'])
+    # This first one will fail due to pre-commit hook failing, which
+    # will reformat some files to be compliant.
+    subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+    # non-compliant files have been modified, so just commit -a
+    subprocess.call(['git', 'commit', '-a', '-m', 'Initial commit'])
+
+    version = "{{cookiecutter.version}}"
+    subprocess.call(['git', 'tag', '-a', version, '-m', 'Initial commit'])
