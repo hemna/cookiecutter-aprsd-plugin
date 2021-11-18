01ec699edf40b990c2032d258765f106e9ba1442
Hemna <waboring@hemna.com>
2021-11-18 12:05:05 -0500

    Update docs in code, changed plugin name

--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/{{cookiecutter.module_name}}.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/{{cookiecutter.module_name}}.py
@@ -11,14 +11,23 @@ LOG = logging.getLogger("APRSD")
 class {{cookiecutter.plugin_class_name}}(plugin.{{cookiecutter.plugin_parent_object}}):
 {% if cookiecutter.plugin_parent_object == "APRSDRegexCommandPluginBase" %}
     version = {{cookiecutter.module_name}}.__version__
+    # Change this regex to match for your plugin's command
+    # Tutorial on regex here: https://regexone.com/
     # Look for any command that starts with w or W
     command_regex = "^[wW]"
     # the command is for ?
+    # Change this value to a 1 word description of the plugin
+    # this string is used for help
     command_name = "weather"
 {% endif %}
     enabled = False
 
     def setup(self):
+        """Allows the plugin to do some 'setup' type checks in here.
+
+        If the setup checks fail, set the self.enabled = False.  This
+        will prevent the plugin from being called when packets are
+        received."""
         # Do some checks here?
         self.enabled = True
 
@@ -41,18 +50,20 @@ class {{cookiecutter.plugin_class_name}}(plugin.{{cookiecutter.plugin_parent_obj
     @trace.trace
     def process(self, packet):
 {% if cookiecutter.plugin_parent_object == "APRSDRegexCommandPluginBase" %}
-        """This is called when a received packet matches self.command_regex."""
+        """This is called when a received packet matches self.command_regex.
+        
+        This is only called when self.enabled = True and the command_regex
+        matches in the contents of the packet["message_text"]."""
 {% elif cookiecutter.plugin_parent_object == "APRSDWatchListPluginBase" %}
-        """This is called when a received packet matches the watchlist."""
+        """This is called when a received packet matches the watchlist.
+        
+        This is only called when self.enabled = True and callsign in 
+        packet["from"] is in the config's watchlist setting."""
 {% endif %}
         LOG.info("{{cookiecutter.plugin_class_name}} Plugin")
 
         from_callsign = packet.get("from")
         message = packet.get("message_text", None)
 
-        if self.enabled:
-            # Now we can process
-            return "some reply message"
-        else:
-            LOG.warning("{{cookiecutter.plugin_class_name}} is disabled.")
-            return messaging.NULL_MESSAGE
+        # Now we can process
+        return "some reply message"
